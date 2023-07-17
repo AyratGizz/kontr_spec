808aea16b719ecf0005ec82d3a77fda692cd8208
AyratGizz <gizzatullin-airat@mail.ru>
2023-07-17 19:48:04 +0500

    add file_operation.py

--- Note.py
+++ Note.py
@@ -3,7 +3,8 @@ import uuid
 
 
 class Note:
-    def __init__(self, id = str(uuid.uuid1())[0:3],  title = "текст", body = "текст", date = str(datetime.now().strftime("%d.%m.%Y %H:%M:%S"))):
+    def __init__(self, id=str(uuid.uuid1())[0:3], title="текст", body="текст",
+                 date=str(datetime.now().strftime("%d.%m.%Y %H:%M:%S"))):
         self.id = id
         self.title = title
         self.body = body
@@ -37,4 +38,7 @@ class Note:
         return note.id + ';' + note.title + ';' + note.body + ';' + note.date
 
     def map_note(note):
-        return '\nID: ' + note.id + '\n' + 'Название: ' + note.title + '\n' + 'Описание: ' + note.body + '\n' + 'Дата записи: ' + note.date
+        return '\nID: ' + note.id + '\n' + \
+               'Название: ' + note.title + '\n' + \
+               'Описание: ' + note.body + '\n' + \
+               'Дата записи: ' + note.date
